8e37a59a6538b69df07c85409a60135b65b13661
Igor Broychenko <qatester888it@gmail.com>
2017-12-06 13:08:38 +0200

    Add acv variable

--- src/test/java/task1/Arrays1.java
+++ src/test/java/task1/Arrays1.java
@@ -7,7 +7,7 @@ public class Arrays1 {
     @Test
     public void numbers(){
         int a = 12;
-        int b = 101;
+        int b = 14;
         int c = a + b;
 	int d = c + a;	
         System.out.println(c);
