04b5826959a50e9461109e65c2fe84068a0f1e4c
Igor Broychenko <qatester888it@gmail.com>
2017-12-06 15:50:02 +0200

    Complete task

--- src/test/java/task1/Arrays1.java
+++ src/test/java/task1/Arrays1.java
@@ -9,11 +9,12 @@ public class Arrays1 {
         int a = 12;
         int b = 14;
         int c = a + b;
-	int d = c + a;	
-         int e = c + a;	
-	String str = "Finaly I set up Jenkins";
+	    int d = c + a;
+        int e = c + a;
+        int r = a + b + c + d + e;
+	    String str = "Finaly I set up Jenkins after ";
 
 
-        System.out.println(c);
+        System.out.println(str + r+ " attemps");
     }
 }
