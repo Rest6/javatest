d7a0668196998c097be95eabe22b12800486bda0
Igor Broychenko <qatester888it@gmail.com>
2017-12-06 13:01:04 +0200

    Add ac variable

--- src/test/java/task1/Arrays1.java
+++ src/test/java/task1/Arrays1.java
@@ -7,7 +7,7 @@ public class Arrays1 {
     @Test
     public void numbers(){
         int a = 12;
-        int b = 10;
+        int b = 101;
         int c = a + b;
 	int d = c + a;	
         System.out.println(c);
