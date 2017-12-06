a5850687c878456a6aa3f1f98039f88802bb38ee
Igor Broychenko <qatester888it@gmail.com>
2017-12-06 12:27:24 +0200

    Java project

---
+++ src/test/java/task1/Arrays1.java
@@ -0,0 +1,15 @@
+package task1;
+
+import org.junit.Test;
+
+public class Arrays1 {
+
+    @Test
+    public void numbers(){
+        int a = 12;
+        int b = 10;
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
